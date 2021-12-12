b5614d7e2ff0e16503c6a6f5cb6d87018748760e
Christophe Lafenetre <christophe.lafenetre@gmail.com>
2021-12-12 10:06:27 +0100

    finish game ! without css

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,39 +2,32 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    # @letters = (0..10).map{  }
-    # ('A'..'Z').sample
-    @letters = []
-    9.times { @letters << ('A'..'Z').to_a.sample }
-    @letters
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
+
+  def score
+    @user_word = params[:word]
+    @response = valid_word(@user_word) # {"found"=>true, "word"=>"teacher", "length"=>7}
+    @letters = params['letters']
+    @valid_word = valid_word(@user_word)
+    @valid_letter = valid_letter(@user_word, @letters)
+  end
+
+  private
 
   def valid_word(word)
     @url = 'https://wagon-dictionary.herokuapp.com/'
-    JSON.parse(URI(@url + word).read)
+    @result = JSON.parse(URI(@url + word).read)
+    @result['found'] # true or false
   end
 
   def valid_letter(word, letters)
     word.upcase!
     word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
-  end
-
-  def score
-    # mot correspond aux lettres donnée
-    # mot valid avec le dico anglais
-    # recup mot inséré
-    @user_word = params[:word]
-    @response = valid_word(@user_word) # {"found"=>true, "word"=>"teacher", "length"=>7}
-    @letters = params['letters']
-    p valid_letter(@user_word, @letters)
-    unless valid_letter(@user_word, @letters)
-      # if true return le bon affichage
-      @message = "mot correct"
-    elsif @response["found"] == true
-      # affiche erreur mot pas anglais
-      @message = "mot pas bon"
-    end
-    use fonction valid
   end
 end
